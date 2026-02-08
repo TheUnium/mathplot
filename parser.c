d493fc2861510557510a0f059ee461fa3a740016
TheUnium <81354905+TheUnium@users.noreply.github.com>
2026-02-08 11:53:27 +0530

    parser 2 plus pro max

--- parser.c
+++ parser.c
@@ -3,198 +3,378 @@
 #include "parser.h"
 #include <ctype.h>
 #include <math.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static double parse_expr(const char **p, double x);
-static double parse_term(const char **p, double x);
-static double parse_factor(const char **p, double x);
-static double parse_power(const char **p, double x);
+static double parse_expr(const char **p, double x, int *e);
+static double parse_term(const char **p, double x, int *e);
+static double parse_factor(const char **p, double x, int *e);
+static double parse_power(const char **p, double x, int *e);
+static double parse_unary(const char **p, double x, int *e);
+static void swsp(const char **p);
+static double factorial(double n);
 
-static double parse_power(const char **p, double x) {
-  while (isspace(**p))
+static void swsp(const char **p) {
+  while (isspace(**p)) {
     (*p)++;
+  }
+}
+
+static double factorial(double n) {
+  if (n < 0 || n != floor(n))
+    return NAN;
+  if (n > 170)
+    return INFINITY;
+  if (n == 0 || n == 1)
+    return 1.0;
+  double result = 1.0;
+  for (int i = 2; i <= (int)n; i++)
+    result *= i;
+  return result;
+}
+
+static int cstrncasecmp(const char *s1, const char *s2, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    int c1 = tolower((unsigned char)s1[i]);
+    int c2 = tolower((unsigned char)s2[i]);
+    if (c1 != c2)
+      return c1 - c2;
+    if (c1 == '\0')
+      return 0;
+  }
+  return 0;
+}
+
+static double parse_atom(const char **p, double x, int *e) {
+  swsp(p);
 
   if (**p == '(') {
     (*p)++;
-    double val = parse_expr(p, x);
-    while (isspace(**p))
+    double val = parse_expr(p, x, e);
+    if (*e)
+      return NAN;
+    swsp(p);
+    if (**p == ')') {
       (*p)++;
-    if (**p == ')')
-      (*p)++;
+    } else {
+      *e = 1;
+      return NAN;
+    }
     return val;
   }
 
-  if (**p == 'x') {
+  if (**p == 'x' || **p == 'X') {
     (*p)++;
     return x;
   }
 
-  if (strncmp(*p, "pi", 2) == 0 || strncmp(*p, "PI", 2) == 0) {
-    *p += 2;
-    return M_PI;
-  }
-  if (**p == 'e' && !isalpha(*(*p + 1))) {
-    (*p)++;
-    return M_E;
+  if (cstrncasecmp(*p, "pi", 2) == 0) {
+    if (!isalpha(*(*p + 2))) {
+      *p += 2;
+      return M_PI;
+    }
   }
 
-  if (strncmp(*p, "sin", 3) == 0) {
+  if ((**p == 'e' || **p == 'E')) {
+    char next = *(*p + 1);
+    if (!isalpha(next)) {
+      (*p)++;
+      return M_E;
+    }
+  }
+
+  if (cstrncasecmp(*p, "asin", 4) == 0 && !isalpha(*(*p + 4))) {
+    *p += 4;
+    double arg = parse_unary(p, x, e);
+    if (*e)
+      return NAN;
+    if (arg < -1.0 || arg > 1.0)
+      return NAN;
+    return asin(arg);
+  }
+  if (cstrncasecmp(*p, "acos", 4) == 0 && !isalpha(*(*p + 4))) {
+    *p += 4;
+    double arg = parse_unary(p, x, e);
+    if (*e)
+      return NAN;
+    if (arg < -1.0 || arg > 1.0)
+      return NAN;
+    return acos(arg);
+  }
+  if (cstrncasecmp(*p, "atan", 4) == 0 && !isalpha(*(*p + 4))) {
+    *p += 4;
+    return atan(parse_unary(p, x, e));
+  }
+  if (cstrncasecmp(*p, "sinh", 4) == 0 && !isalpha(*(*p + 4))) {
+    *p += 4;
+    return sinh(parse_unary(p, x, e));
+  }
+  if (cstrncasecmp(*p, "cosh", 4) == 0 && !isalpha(*(*p + 4))) {
+    *p += 4;
+    return cosh(parse_unary(p, x, e));
+  }
+  if (cstrncasecmp(*p, "tanh", 4) == 0 && !isalpha(*(*p + 4))) {
+    *p += 4;
+    return tanh(parse_unary(p, x, e));
+  }
+  if (cstrncasecmp(*p, "sin", 3) == 0 && !isalpha(*(*p + 3))) {
     *p += 3;
-    return sin(parse_power(p, x));
+    return sin(parse_unary(p, x, e));
   }
-  if (strncmp(*p, "cos", 3) == 0) {
+  if (cstrncasecmp(*p, "cos", 3) == 0 && !isalpha(*(*p + 3))) {
     *p += 3;
-    return cos(parse_power(p, x));
+    return cos(parse_unary(p, x, e));
   }
-  if (strncmp(*p, "tan", 3) == 0) {
+  if (cstrncasecmp(*p, "tan", 3) == 0 && !isalpha(*(*p + 3))) {
     *p += 3;
-    return tan(parse_power(p, x));
+    return tan(parse_unary(p, x, e));
   }
-  if (strncmp(*p, "asin", 4) == 0) {
-    *p += 4;
-    return asin(parse_power(p, x));
-  }
-  if (strncmp(*p, "acos", 4) == 0) {
-    *p += 4;
-    return acos(parse_power(p, x));
-  }
-  if (strncmp(*p, "atan", 4) == 0) {
-    *p += 4;
-    return atan(parse_power(p, x));
-  }
-  if (strncmp(*p, "sinh", 4) == 0) {
-    *p += 4;
-    return sinh(parse_power(p, x));
-  }
-  if (strncmp(*p, "cosh", 4) == 0) {
-    *p += 4;
-    return cosh(parse_power(p, x));
-  }
-  if (strncmp(*p, "tanh", 4) == 0) {
-    *p += 4;
-    return tanh(parse_power(p, x));
-  }
-  if (strncmp(*p, "exp", 3) == 0) {
+  if (cstrncasecmp(*p, "exp", 3) == 0 && !isalpha(*(*p + 3))) {
     *p += 3;
-    return exp(parse_power(p, x));
+    return exp(parse_unary(p, x, e));
   }
-  if (strncmp(*p, "ln", 2) == 0) {
+  if (cstrncasecmp(*p, "sqrt", 4) == 0 && !isalpha(*(*p + 4))) {
+    *p += 4;
+    double arg = parse_unary(p, x, e);
+    if (*e)
+      return NAN;
+    if (arg < 0.0)
+      return NAN;
+    return sqrt(arg);
+  }
+  if (cstrncasecmp(*p, "ln", 2) == 0 && !isalpha(*(*p + 2))) {
     *p += 2;
-    return log(parse_power(p, x));
+    double arg = parse_unary(p, x, e);
+    if (*e)
+      return NAN;
+    if (arg <= 0.0)
+      return NAN;
+    return log(arg);
   }
-  if (strncmp(*p, "log", 3) == 0) {
+  if (cstrncasecmp(*p, "log", 3) == 0 && !isalpha(*(*p + 3))) {
     *p += 3;
-    return log10(parse_power(p, x));
+    double arg = parse_unary(p, x, e);
+    if (*e)
+      return NAN;
+    if (arg <= 0.0)
+      return NAN;
+    return log10(arg);
   }
-  if (strncmp(*p, "sqrt", 4) == 0) {
-    *p += 4;
-    return sqrt(parse_power(p, x));
-  }
-  if (strncmp(*p, "abs", 3) == 0) {
+  if (cstrncasecmp(*p, "abs", 3) == 0 && !isalpha(*(*p + 3))) {
     *p += 3;
-    return fabs(parse_power(p, x));
+    return fabs(parse_unary(p, x, e));
   }
-  if (strncmp(*p, "floor", 5) == 0) {
+  if (cstrncasecmp(*p, "floor", 5) == 0 && !isalpha(*(*p + 5))) {
     *p += 5;
-    return floor(parse_power(p, x));
+    return floor(parse_unary(p, x, e));
   }
-  if (strncmp(*p, "ceil", 4) == 0) {
+  if (cstrncasecmp(*p, "ceil", 4) == 0 && !isalpha(*(*p + 4))) {
     *p += 4;
-    return ceil(parse_power(p, x));
+    return ceil(parse_unary(p, x, e));
   }
 
-  int sign = 1;
-  if (**p == '-') {
-    sign = -1;
-    (*p)++;
-  } else if (**p == '+') {
-    (*p)++;
-  }
+  if (isdigit(**p) || **p == '.') {
+    double val = 0;
+    int has_digits = 0;
 
-  double val = 0;
-  int has_digits = 0;
-
-  while (isdigit(**p)) {
-    val = val * 10 + (**p - '0');
-    (*p)++;
-    has_digits = 1;
-  }
-
-  if (**p == '.') {
-    (*p)++;
-    double frac = 0.1;
     while (isdigit(**p)) {
-      val += (**p - '0') * frac;
-      frac *= 0.1;
+      val = val * 10 + (**p - '0');
       (*p)++;
       has_digits = 1;
     }
-  }
 
-  if (!has_digits)
-    return 0;
-  return sign * val;
-}
-
-static double parse_factor(const char **p, double x) {
-  double val = parse_power(p, x);
-  while (isspace(**p))
-    (*p)++;
-  while (**p == '^') {
-    (*p)++;
-    val = pow(val, parse_power(p, x));
-    while (isspace(**p))
+    if (**p == '.') {
       (*p)++;
-  }
-  return val;
-}
-
-static double parse_term(const char **p, double x) {
-  double val = parse_factor(p, x);
-  while (isspace(**p))
-    (*p)++;
-  while (**p == '*' || **p == '/' || isalnum(**p)) {
-    char op = **p;
-    if (op == '*') {
-      (*p)++;
-      val *= parse_factor(p, x);
-    } else if (op == '/') {
-      (*p)++;
-      double divisor = parse_factor(p, x);
-      if (fabs(divisor) < 1e-10)
-        return NAN;
-      val /= divisor;
-    } else {
-      val *= parse_factor(p, x);
+      double frac = 0.1;
+      while (isdigit(**p)) {
+        val += (**p - '0') * frac;
+        frac *= 0.1;
+        (*p)++;
+        has_digits = 1;
+      }
     }
-    while (isspace(**p))
+    if ((**p == 'e' || **p == 'E') && has_digits) {
+      const char *s = *p;
       (*p)++;
+      int exp_sign = 1;
+      if (**p == '-') {
+        exp_sign = -1;
+        (*p)++;
+      } else if (**p == '+') {
+        (*p)++;
+      }
+
+      int exponent = 0;
+      int has_exp_digits = 0;
+      while (isdigit(**p)) {
+        exponent = exponent * 10 + (**p - '0');
+        (*p)++;
+        has_exp_digits = 1;
+      }
+
+      if (has_exp_digits) {
+        val *= pow(10, exp_sign * exponent);
+      } else {
+        *p = s;
+      }
+    }
+
+    if (!has_digits) {
+      *e = 1;
+      return NAN;
+    }
+    return val;
   }
+
+  *e = 1;
+  return NAN;
+}
+
+static double parse_pf(const char **p, double x, int *error) {
+  double val = parse_atom(p, x, error);
+  if (*error)
+    return NAN;
+
+  swsp(p);
+
+  while (**p == '!') {
+    (*p)++;
+    val = factorial(val);
+    if (isnan(val) || isinf(val)) {
+      *error = 1;
+      return NAN;
+    }
+    swsp(p);
+  }
+
   return val;
 }
 
-static double parse_expr(const char **p, double x) {
-  double val = parse_term(p, x);
-  while (isspace(**p))
+static double parse_power(const char **p, double x, int *e) {
+  double val = parse_pf(p, x, e);
+  if (*e)
+    return NAN;
+
+  swsp(p);
+
+  if (**p == '^') {
     (*p)++;
+    double exponent = parse_power(p, x, e);
+    if (*e)
+      return NAN;
+    val = pow(val, exponent);
+  }
+
+  return val;
+}
+
+static double parse_unary(const char **p, double x, int *e) {
+  swsp(p);
+
+  if (**p == '-') {
+    (*p)++;
+    return -parse_unary(p, x, e);
+  } else if (**p == '+') {
+    (*p)++;
+    return parse_unary(p, x, e);
+  }
+
+  return parse_power(p, x, e);
+}
+
+static double parse_factor(const char **p, double x, int *e) {
+  double val = parse_unary(p, x, e);
+  if (*e)
+    return NAN;
+
+  swsp(p);
+
+  while (**p == '*' || **p == '/' || **p == '%' || isalnum(**p) || **p == '(') {
+    if (**p == '*') {
+      (*p)++;
+      double rhs = parse_unary(p, x, e);
+      if (*e)
+        return NAN;
+      val *= rhs;
+    } else if (**p == '/') {
+      (*p)++;
+      double divisor = parse_unary(p, x, e);
+      if (*e)
+        return NAN;
+      if (fabs(divisor) < 1e-15) {
+        *e = 1;
+        return NAN;
+      }
+      val /= divisor;
+    } else if (**p == '%') {
+      (*p)++;
+      double divisor = parse_unary(p, x, e);
+      if (*e)
+        return NAN;
+      if (fabs(divisor) < 1e-15) {
+        *e = 1;
+        return NAN;
+      }
+      val = fmod(val, divisor);
+    } else {
+      double rhs = parse_unary(p, x, e);
+      if (*e)
+        return NAN;
+      val *= rhs;
+    }
+
+    swsp(p);
+  }
+
+  return val;
+}
+
+static double parse_term(const char **p, double x, int *e) {
+  double val = parse_factor(p, x, e);
+  if (*e)
+    return NAN;
+
+  swsp(p);
+
   while (**p == '+' || **p == '-') {
     char op = **p;
     (*p)++;
+    double rhs = parse_factor(p, x, e);
+    if (*e)
+      return NAN;
+
     if (op == '+')
-      val += parse_term(p, x);
+      val += rhs;
     else
-      val -= parse_term(p, x);
-    while (isspace(**p))
-      (*p)++;
+      val -= rhs;
+
+    swsp(p);
   }
+
   return val;
+}
+
+static double parse_expr(const char **p, double x, int *e) {
+  return parse_term(p, x, e);
 }
 
 double p_eval(const char *expr, double x) {
   if (!expr || strlen(expr) == 0)
     return NAN;
+
   const char *p = expr;
-  return parse_expr(&p, x);
+  int e = 0;
+
+  double result = parse_expr(&p, x, &e);
+
+  if (e)
+    return NAN;
+
+  swsp(&p);
+  if (*p != '\0')
+    return NAN;
+
+  return result;
 }
